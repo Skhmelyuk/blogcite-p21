8bc60a2714d132e4ac9b66e6c518baa4b6b16543
Хмелюк Сергій Іванович <Hmelyuk@step.local>
2026-01-28 21:14:22 +0200

    lesson-02 django

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,17 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Category(models.Model):
+  name = models.CharField(max_length=50, db_index=True, verbose_name="Ім'я категорії")
+  slug = models.SlugField(max_length=50, unique=True, verbose_name="Слаг")
+
+  class Meta:
+      ordering = ["name"]
+      verbose_name = "Категорія"
+      verbose_name_plural = "Категорії"
+
+  def __str__(self):
+      return self.name
+  
+  def get_absolute_url(self):
+      return reverse("main:post_list_by_category", args=[self.slug])
 
 class Post(models.Model):
-  title = models.CharField(max_length=200)
-  content = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  likes = models.IntegerField(default=0)
-  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категорія", null=True, blank=True)
+  title = models.CharField(max_length=100, db_index=True, verbose_name="Заголовок")
+  slug = models.SlugField(max_length=100, unique=True, verbose_name="Слаг")
+  image = models.ImageField(upload_to="posts/%Y/%m/%d/", blank=True, verbose_name="Зображення")
+  content = models.TextField(verbose_name="Контент")
+  created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата створення")
+  updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата оновлення")
+  likes = models.IntegerField(default=0, verbose_name="Лайки")
+  views = models.IntegerField(default=0, verbose_name="Перегляди")
+  author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
+
+  class Meta:
+      ordering = ["-created_at"]
+      verbose_name = "Пост"
+      verbose_name_plural = "Пости"
 
   def __str__(self):
       return f" {self.title} - { self.created_at }"
   
-class Categories(models.Model):
-   pass
-  
+  def get_absolute_url(self):
+      return reverse("main:post_detail", args=[self.id, self.slug])
